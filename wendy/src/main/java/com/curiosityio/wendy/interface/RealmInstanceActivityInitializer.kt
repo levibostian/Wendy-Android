95032e63b6b1c192449045d4bfa4cc7a4cbe36f9
Levi Bostian <levi@curiosityio.com>
2017-03-30 07:02:06 -0500

    Add docs on how to use RealmInstanceActivityIninitializer.

--- wendy/src/main/java/com/curiosityio/wendy/interface/RealmInstanceActivityInitializer.kt
+++ wendy/src/main/java/com/curiosityio/wendy/interface/RealmInstanceActivityInitializer.kt
@@ -6,16 +6,61 @@ import io.realm.Realm
 
 interface RealmInstanceActivityInitializer {
 
+    // Note: I may get rid of this eventually. I use it for bad practice usually.
     interface RealmClosedListener {
         fun beforeRealmClose()
         fun afterRealmClose()
     }
 
+    // Usually do this in onCreate?
+    // uiRealm = RealmInstanceManager.getInstance()
+    //for (BaseDataManager baseDataManager : mRegisteredDataManagers) {
+    //  baseDataManager.setUiRealm(realm)
+    //}
+    //mRegisteredDataManagers = new ArrayList<>()
     var uiRealm: Realm?
 
+    /*
+    public void registerDataManagers(BaseDataManager... baseDataManagers) {
+        if (uiRealm != null) {
+            for (BaseDataManager dataManager : baseDataManagers) {
+                dataManager.setUiRealm(realm)
+            }
+        } else {
+            Collections.addAll(mRegisteredDataManagers, baseDataManagers);
+        }
+    }
+     */
     fun registerDataManagers(vararg baseDataManagers: BaseWendyDataManager)
 
+    /*
+    private ArrayList<RealmClosedListener> mRealmClosedListeners = new ArrayList<>()
+
+    public void addRealmClosedListener(RealmClosedListener realmClosedListener) {
+        mRealmClosedListeners.add(realmClosedListener)
+    }
+     */
     fun addRealmClosedListener(realmClosedListener: RealmClosedListener)
+
+    /*
+    private void closeRealmInstance() {
+        if (realm != null) {
+            for (RealmClosedListener listener : mRealmClosedListeners) {
+                listener.beforeRealmClose();
+            }
+
+            realm.close();
+
+            for (RealmClosedListener listener : mRealmClosedListeners) {
+                listener.afterRealmClose();
+            }
+
+            mRealmClosedListeners = new ArrayList<>();
+        }
+    }
+     */
+    // Call in onDestroy()
+    fun closeRealmInstance()
 
 }
 
